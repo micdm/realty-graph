5ff6650f00a777a16be8dd3669d06d007639814d
Mic <mic-dm@mail.ru>
2012-08-14 23:43:52 +0700

    rmk: подсчитываем количество новых объявлений

--- src/get_source_data.py
+++ src/get_source_data.py
@@ -17,7 +17,7 @@ from dmte.processors import AdvertProcessor
 def save_advert(advert):
     '''
     Сохраняет объявление в БД.
-    Возвращает True, если объявление новое.
+    Возвращает, новое ли объявление.
     @param advert: Advert
     @return: bool
     '''
@@ -27,29 +27,34 @@ def save_advert(advert):
         logger.debug('advert with external id %s found', advert.external_id)
         advert.id = stored.id
     processor.save(advert)
+    logger.debug('advert with external id %s saved', advert.external_id)
     return stored is None
 
-def parse_page(url, new_only):
+def save_adverts(adverts):
+    '''
+    Сохраняет объявления.
+    Возвращает количество новых.
+    @param adverts: list
+    @return: int
+    '''
+    count = 0
+    for advert in adverts:
+        if save_advert(advert):
+            count += 1
+    return count
+
+def parse_page(url):
     '''
     Разбирает страницу на объявления.
-    Возвращает адрес следующей страницы.
+    Возвращает список найденных объявлений и адрес следующей страницы.
     @param url: str
-    @param new_only: bool
-    @return: str
+    @return: list, str
     '''
     logger.debug('parsing adverts on %s', url)
     content = get_page_content(url)
     root_node = etree.fromstring(content, parser=etree.HTMLParser())
     parser = AdvertListParser()
-    has_new = False
-    for advert in parser.parse_adverts(root_node):
-        logger.debug('advert %s found', str(advert))
-        if save_advert(advert):
-            has_new = True
-    if new_only and not has_new:
-        logger.debug('no new adverts found on the page %s, stopping', url)
-        return None        
-    return parser.parse_next_page_address(root_node)
+    return parser.parse_adverts(root_node), parser.parse_next_page_address(root_node)
 
 def parse_all(new_only=False):
     '''
@@ -57,13 +62,24 @@ def parse_all(new_only=False):
     @param new_only: bool
     '''
     if new_only:
-        logger.debug('parsing only new adverts')
+        logger.info('parsing new adverts only')
     else:
-        logger.debug('parsing all adverts')
+        logger.info('parsing all adverts')
     next_url = '/realty/?type=1&otype=1&listview=1&perpage=200'
-    while next_url:
-        next_url = parse_page('http://www.tomsk.ru09.ru%s'%next_url, new_only)
+    total_count = 0
+    while True:
+        adverts, next_url = parse_page('http://www.tomsk.ru09.ru%s'%next_url)
+        count = save_adverts(adverts)
+        logger.debug('%s new adverts found on page', count)
+        total_count += count
+        if new_only and count == 0:
+            logger.info('no new adverts found on page, stopping')
+            break
+        if next_url is None:
+            logger.info('last page reached, stopping')
+            break
         sleep(1)
+    logger.info('%s new adverts found', total_count)
 
 new_only = len(sys.argv) > 1 and sys.argv[1] == 'new'
 parse_all(new_only)
